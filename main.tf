567f30965e7d56ad65c738f2d584623a4e6975ee
SUNIL GADHE <89225052+sunilgadhe@users.noreply.github.com>
2025-05-16 11:46:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-} 
-
 resource "aws_instance" "web" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
