91825f52218221c8a1cf66253656f20e6a3d0fdd
SUNIL GADHE <89225052+sunilgadhe@users.noreply.github.com>
2025-05-16 11:46:31 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,11 +5,11 @@ terraform {
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
   }
- # required_version = ">=0.13.0"
-  #required_providers {
-   # aws = {
-    #  version = ">= 2.7.0"
-     # source = "hashicorp/aws"
-    #}
-  #}
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
 }
